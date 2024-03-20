5e56d723878eac1782396353a33cf37c7141a83b
sharma66mahesh <gauravpudel1990@gmail.com>
2024-03-20 22:17:47 +0545

    Share common code between popup and content script

--- contentScript.js
+++ contentScript.js
@@ -1,36 +1,6 @@
-var currentTheme = 0; // 0 -> light, 1 -> dark
-var overlayId = "ovrly-398592834928817358";
-
+// Listen for user actions
 chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
-  console.log("msg from popup: ", msg);
-  toggleTheme(msg.prevTheme);
-})
-
-function toggleTheme(currentTheme) {
-  if (currentTheme == 0) {
-    const overlay = document.createElement("div");
-
-    const css = `
-      position: fixed;
-      pointer-events: none;
-      top: 0;
-      left: 0;
-      width: 100vw;
-      height: 100vh;
-      background-color: white;
-      mix-blend-mode: difference;
-      z-index: 1;
-  `;
-    overlay.setAttribute("style", css);
-    overlay.setAttribute("id", overlayId);
-
-    document.body.appendChild(overlay);
-
-    currentTheme = 1;
-  } else {
-    const overlay = document.getElementById(overlayId);
-    overlay.parentNode.removeChild(overlay);
-
-    currentTheme = 0;
+  if (msg.toggle) {
+    toggleTheme(true);
   }
-}
+});
--- js/script.js
+++ js/script.js
@@ -1,49 +1,15 @@
 (function addEventListeners() {
   console.log("LOADED>>>>>>>>>>>>>>>>>>>>>>>>>>");
-  document.getElementById("switch").onclick = toggleTheme;
+  document.getElementById("switch").onclick = () => {
+    toggleTheme(true);
+    sendToggleMessage();
+  }
 })();
 
-var currentTheme = 0; // 0 -> light, 1 -> dark
-var overlayId = "ovrly-398592834928817358";
-
-function toggleTheme() {
-  console.log("Toggling theme...");
-
-  sendToggleMessage(currentTheme);
-
-  if (currentTheme == 0) {
-    const overlay = document.createElement("div");
-
-    const css = `
-      position: fixed;
-      pointer-events: none;
-      top: 0;
-      left: 0;
-      width: 100vw;
-      height: 100vh;
-      background-color: white;
-      mix-blend-mode: difference;
-      z-index: 1;
-  `;
-    overlay.setAttribute("style", css);
-    overlay.setAttribute("id", overlayId);
-
-    document.body.appendChild(overlay);
-
-    currentTheme = 1;
-  } else {
-    const overlay = document.getElementById(overlayId);
-    overlay.parentNode.removeChild(overlay);
-
-    currentTheme = 0;
-  }
-}
-
-function sendToggleMessage(currentTheme) {
+function sendToggleMessage() {
   console.log("TOGGLING...");
   chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
     await chrome.tabs.sendMessage(tabs[0].id, {
-      prevTheme: currentTheme,
       toggle: true,
     });
   });
