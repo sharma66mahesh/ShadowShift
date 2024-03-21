89762f1290770a99b2353b168f9577e1963c896f
Kishor Neupane <kishorliv@gmail.com>
2024-03-21 22:45:23 +1100

    refactor: load and the theme to the page

--- content-script.js
+++ content-script.js
@@ -1,10 +1,18 @@
+// load and apply saved theme for the webpage
+chrome.storage.sync.get("theme", function (data) {
+  if (data.theme) {
+    applyTheme(data.theme);
+  }
+});
+
 // listen for events from the extension popup
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request) => {
   if (request.theme) {
     applyTheme(request.theme);
   }
 });
 
+// apply theme to the current tab/webpage
 function applyTheme(theme) {
   if (theme === "light") {
     document.body.style.backgroundColor = "white";
